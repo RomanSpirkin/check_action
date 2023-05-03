079cab1f809619aa3e6f5aa14b68d58187a29663
Roman Spirkin <rospirkin@yandex.ru>
2023-05-03 12:44:12 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-
+#include "build/version.h"
 
 int main(){
-    std::cout << "Version: " << "PROJECT_VERSION_PATCH "<< std::endl;
+    std::cout << "Version: " << PATCH_VERSION << std::endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
